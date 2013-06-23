9d6facd78ba6d40a12220725bc95240653bc82b3
NdrsPtrw <andreas.petrow@st.ovgu.de>
2013-06-23 16:56:33 +0200

    buttons onclick
    Die Methoden für die buttons sind drin und verlinken auf die jeweilige
    seite.
    hab auch ne methode geunden die die textinfo abliest, ist aber nicht
    getestet worden.

--- src/de/ovgu/swe_projekt/socialdoc/MainActivity.java
+++ src/de/ovgu/swe_projekt/socialdoc/MainActivity.java
@@ -3,13 +3,20 @@ package de.ovgu.swe_projekt.socialdoc;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+                                                            /*Hier wird die App gestartet
+                                                            abfrageob ein User bereits existiert
+                                                            und dann den pasenden View auszusuchen
+                                                             */
+        setContentView(R.layout.mainmenu);
+        //setContentView(R.layout.proband_code);
+
     }
 
 
@@ -19,5 +26,30 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+                                                /*
+                                                Wie man an die textinformationen kommt:
+                                                EditText editText = (EditText) findViewById(R.id.<textfield>);
+                                                editText.getText
+                                                 */
+    public void button_menu_to_quest(View view){
+        setContentView(R.layout.question);
+    }
+    public void button_menu_to_time(View view){
+        setContentView(R.layout.set_time);
+    }
+    public void button_menu_to_settings(View view){
+        setContentView(R.layout.settings);
+    }
+    public void button_question_ok(View view){
+        setContentView(R.layout.mainmenu);
+    }
+    public void button_time_ok(View view){
+        setContentView(R.layout.mainmenu);
+    }
+    public void button_settings_ok(View view){
+        setContentView(R.layout.mainmenu);
+    }
+    public void button_proband_ok(View view){
+        setContentView(R.layout.mainmenu);
+    }
 }

410a2933ceaf098a612b55ea5e044bc088c30800
NdrsPtrw <andreas.petrow@st.ovgu.de>
2013-06-30 17:26:15 +0200

    New

--- src/de/ovgu/swe_projekt/socialdoc/MainActivity.java
+++ src/de/ovgu/swe_projekt/socialdoc/MainActivity.java
@@ -1,5 +1,8 @@
 package de.ovgu.swe_projekt.socialdoc;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -13,14 +16,19 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-                                                            /*Hier wird die App gestartet
-                                                            abfrageob ein User bereits existiert
-                                                            und dann den pasenden View auszusuchen
-                                                             */
+        /*Hier wird die App gestartet
+        abfrageob ein User bereits existiert
+        und dann den pasenden View auszusuchen
+         */
 
         // set up control
         _control = new Control(getSharedPreferences("PsyAppPreferences", 0));
         // TODO: also check if user exists on show, not only on create
+
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
         if( _control.createCSV() )
             setContentView(R.layout.mainmenu);
         else
@@ -29,6 +37,28 @@ public class MainActivity extends Activity {
     @Override
     public void onBackPressed() {
         // do nothing.
+    }
+    @Override       //Fehlermeldungen
+    protected Dialog onCreateDialog(int id){
+        switch(id){
+            case 10:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage(R.string.dialog_message).setTitle(R.string.dialog_title);
+                builder.setCancelable(true);
+                builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User clicked OK button
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+
+        }
+
+
+        return super.onCreateDialog(id);
     }
 
     @Override
@@ -40,34 +70,56 @@ public class MainActivity extends Activity {
 
     public void button_menu_to_quest(View view){
         setContentView(R.layout.question);
+        setQuestion("heute schon");
     }
     public void button_menu_to_time(View view){
         setContentView(R.layout.set_time);
     }
     public void button_question_ok(View view){
+        // todo: check for wrong input! And show a popup and request new input if required!
         String numContacts, numHours, numMinutes;
         numContacts = getEditText(R.id.editText_contacts);
         numHours = getEditText(R.id.editText_hours);
         numMinutes = getEditText(R.id.editText_minutes);
 
-        _control.saveUserInputToCSV(numContacts, numHours, numMinutes);
+        // todo: on alarm alarmsSinceLastAnswered has to be increased by 1
+        // nextAlarmTime has to be set to next
+        _control.changeLastAnsweredAt();
+        _control.saveUserInputToCSV(false, numContacts, numHours, numMinutes);
+        _control.saveUserData(getSharedPreferences("PsyAppPreferences", 0));
 
         //setButtonEnabled(R.id.goto_question_button, false);
 
         setContentView(R.layout.mainmenu);
     }
     public void button_time_ok(View view){
+        // todo: get times
+        _control.saveUserData(getSharedPreferences("PsyAppPreferences", 0));
         setContentView(R.layout.mainmenu);
     }
     public void button_time_back(View view){
         setContentView(R.layout.mainmenu);
     }
     public void button_proband_ok(View view){
+        String code = "";
+        // create probandencode from different parts
+        // ToDo: adjust the view to actually support this and check if all are actually characters
+        // (and not numbers/other symbols)
+        _control.newUser( code );
         setContentView(R.layout.mainmenu);
     }
     private String getEditText(int id){
         EditText editText = (EditText) findViewById(id);
         return editText.getText().toString();
+    }
+    private void setQuestion(String time){
+        String quest = "Mit wie vielen Interaktionspartnern hatten Sie ";
+        quest+=time;
+        quest+=" Kontakt? Schätzen Sie bitte zusätzlich die Gesamtdauer der Kontakte ein.";
+
+
+        TextView text = (TextView) findViewById(R.id.question);
+        text.setText(quest);
     }
     private void setButtonEnabled(int id, boolean enabled){
         Button button = (Button)findViewById(id);
--- src/de/ovgu/swe_projekt/socialdoc/UserData.java
+++ src/de/ovgu/swe_projekt/socialdoc/UserData.java
@@ -7,40 +7,45 @@ import android.content.SharedPreferences;
  */
 public class UserData {
 
+    // the probandencode
     private String _probandenCode = "";
-    private int[] _userTimes = {77,77,77,77};
-    private int _lastAnsweredAt = 77;
-    private Boolean _useSignalLight = false;
-    // missing: sound, volume, light color
+    // the times the alarm is supposed to happen at
+    private int[] _userTimes = {-77,-77,-77,-77};
+    // the time the last answer was given and the amount of alarms since that answer
+    private int _lastAnsweredAt = 0, _currentAlarmTime = 0, _alarmsSinceLastAnswer = 0;
 
     public UserData() {
     }
 
+    // import the user data from the settings
     public void importSavedUserData( SharedPreferences pref) {
         _probandenCode = pref.getString("Probandencode", "");
-        _userTimes[0] = pref.getInt("time1", 77);
-        _userTimes[1] = pref.getInt("time2", 77);
-        _userTimes[2] = pref.getInt("time3", 77);
-        _userTimes[3] = pref.getInt("time4", 77);
-        _lastAnsweredAt = pref.getInt("lastAnswered", 77);
-        _useSignalLight = pref.getBoolean("useSignalLight", false);
+        _userTimes[0] = pref.getInt("time1", -77);
+        _userTimes[1] = pref.getInt("time2", -77);
+        _userTimes[2] = pref.getInt("time3", -77);
+        _userTimes[3] = pref.getInt("time4", -77);
+        _lastAnsweredAt = pref.getInt("lastAnswered", 0);
+        _alarmsSinceLastAnswer =  pref.getInt("alarmsSinceLastAnswered", 0);
     }
 
+    // set user data to default values (for new user)
     public void setUserDataToDefault() {
         _probandenCode = "";
-        _userTimes[0] = 77;
-        _userTimes[1] = 77;
-        _userTimes[2] = 77;
-        _userTimes[3] = 77;
-        _lastAnsweredAt = 77;
-        _useSignalLight = false;
+        _userTimes[0] = -77;
+        _userTimes[1] = -77;
+        _userTimes[2] = -77;
+        _userTimes[3] = -77;
+        _lastAnsweredAt = 0;
+        _alarmsSinceLastAnswer = 0;
+        // todo: set currentAlarmTime to the first userTime that will cause an Alarm (Index of array!)
     }
 
+    // save the user data
     public void saveUserData( SharedPreferences pref) {
         SharedPreferences.Editor preferencesEditor = pref.edit();
-        preferencesEditor.putBoolean("useSignalLight", _useSignalLight);
         preferencesEditor.putString("Probandencode", _probandenCode);
         preferencesEditor.putInt("lastAnswered", _lastAnsweredAt);
+        preferencesEditor.putInt("alarmsSinceLastAnswered", _alarmsSinceLastAnswer);
         preferencesEditor.putInt("time1", _userTimes[0]);
         preferencesEditor.putInt("time2", _userTimes[1]);
         preferencesEditor.putInt("time3", _userTimes[2]);
@@ -51,7 +56,6 @@ public class UserData {
     public String getProbandenCode() {
         return _probandenCode;
     }
-
     public void setProbandenCode(String probandenCode) {
         this._probandenCode = probandenCode;
     }
@@ -59,7 +63,6 @@ public class UserData {
     public int[] getUserTimes() {
         return _userTimes;
     }
-
     public void setUserTimes(int[] userTimes) {
         this._userTimes = userTimes;
     }
@@ -67,16 +70,24 @@ public class UserData {
     public int getLastAnsweredAt() {
         return _lastAnsweredAt;
     }
-
     public void setLastAnsweredAt(int lastAnsweredAt) {
         this._lastAnsweredAt = lastAnsweredAt;
     }
 
-    public Boolean getUseSignalLight() {
-        return _useSignalLight;
+    public int getCurrentAlarmTime() {
+        return this._currentAlarmTime;
+    }
+    public String getStringOfCurrentAlarmTime() {
+        return _userTimes[_currentAlarmTime] + ":00";
     }
 
-    public void setUseSignalLight(Boolean useSignalLight) {
-        this._useSignalLight = useSignalLight;
+    public int getAlarmsSinceLastAnswer() {
+        return _alarmsSinceLastAnswer;
+    }
+    public void resetAlarmsSinceLastAnswer() {
+        this._alarmsSinceLastAnswer = 0;
+    }
+    public void increaseAlarmsSinceLastAnswer() {
+        this._alarmsSinceLastAnswer++;
     }
 }
